ec44dc620af9800c72089a8257b9ccb3a1a31fc5
chankruze <chankruze@gmail.com>
2019-02-05 02:21:14 +0530

    app completed & visually polished

--- app/src/main/java/in/geekofia/miwokapp/NumbersActivity.java
+++ app/src/main/java/in/geekofia/miwokapp/NumbersActivity.java
@@ -18,28 +18,18 @@ public class NumbersActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo’e"));
-        words.add(new Word("ten", "na’aacha"));
+        words.add(new Word("one", "lutti", R.drawable.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight));
+        words.add(new Word("nine", "wo’e", R.drawable.number_nine));
+        words.add(new Word("ten", "na’aacha", R.drawable.number_ten));
 
-//        for (int i = 0; i < words.size(); i++)
-//            Log.v("NumbersActivity","Word at index " + i + " : " + words.get(i));
-
-//        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-//
-//        for (int i = 0; i < words.size(); i++){
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(i));
-//            rootView.addView(wordView);
-//        }
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_numbers);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
--- app/src/main/java/in/geekofia/miwokapp/adapters/WordAdapter.java
+++ app/src/main/java/in/geekofia/miwokapp/adapters/WordAdapter.java
@@ -1,11 +1,13 @@
 package in.geekofia.miwokapp.adapters;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,13 +17,16 @@ import in.geekofia.miwokapp.components.Word;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    /** Resource ID for the background color for this list of words */
+    int mColorResourceId;
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -49,10 +54,27 @@ public class WordAdapter extends ArrayAdapter<Word> {
         defaultTranslation.setText(currentWord.getDefaultTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        // ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_item_image);
         // Get the image resource ID from the current Word object and
         // set the image to iconView
-        // iconView.setImageResource(currentWord.getImageResourceId());
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View categoryBackground = listItemView.findViewById(R.id.categoryBackground);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        // Set the background color of the text container View
+        categoryBackground.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
